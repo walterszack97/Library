34c4d841bd711989dca010322d0a2d3b0efbd4ad
zachary w <walterszack97@gmail.com>
2023-03-31 22:46:17 -0400

    Add function to create book object and add the book to an array of books. Add event listeners to open and close input popup

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, haveRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.haveRead = haveRead;
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(book) {
+  myLibrary.push(book);
 }
+
+const book1 = new Book("A", "A", 1, true);
+const book2 = new Book("B", "A", 2, false);
+const book3 = new Book("C", "A", 3, true);
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+
+function printBooks() {
+  for (i = 0; i < myLibrary.length; i++) {
+    console.log(myLibrary[i].haveRead + "\n");
+  }
+}
+
+printBooks();
+
+//GET DOM ELEMENTS
+const newBook_btn = document.querySelector(".add_btn");
+const newInput_popup = document.querySelector(".newBookInput");
+//EVENT HANDLERS
+newBook_btn.addEventListener("click", (event) => {
+  newInput_popup.style.display = "grid";
+  newBook_btn.style.display = "none";
+});
